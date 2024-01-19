60809b7037328d55b9477e4d0ff9fa276e85f69b
tr1ckste <nikitavasilinenko@gmail.com>
2024-01-19 13:59:30 +0200

    fixed fps

--- av-python/split.py
+++ av-python/split.py
@@ -1,31 +1,37 @@
+import os
 import av
-from PIL import Image
-import time
 from io import BytesIO
+import math
 
 input_file = 'video.mp4'
-output_pattern = 'frames/frame%04d.png'
+output_directory = 'frames'
+output_pattern = 'frame%04d.png'
 desired_fps = 24
 
+# Create output directory if it doesn't exist
+os.makedirs(output_directory, exist_ok=True)
+
 container = av.open(input_file)
-stream = container.streams.video[0]
+video_stream = container.streams.video[0]
 
-# Set the desired fps
-stream.rate = desired_fps
+# Calculate delay between frames based on the desired frame rate
+frameIndexMultiplier = math.ceil(1000 / desired_fps)
 
-start = time.time()
-print("Started extracting frames")
+for packet in container.demux(video=0):
+  for frame in packet.decode():
+    # Calculate the time at which this frame should appear
+    if frame.index % frameIndexMultiplier != 0:
+      continue
 
-for frame in container.decode(video=0):
-  image = frame.to_image()
-    
-  # Convert image to bytes
-  image_bytes = BytesIO()
-  image.save(image_bytes, format='PNG')
-  # Save the image
-  with open(output_pattern % frame.index, 'wb') as f:
-    f.write(image_bytes.getvalue())
+    print(frame.index)
 
-end = time.time()
-print("Frames extracted successfully!")
-print("Time spent in seconds: ", end - start)
+    image = frame.to_image()
+
+    # Convert image to bytes
+    image_bytes = BytesIO()
+    image.save(image_bytes, format='PNG')
+
+    # Save the image
+    output_path = os.path.join(output_directory, output_pattern % frame.index)
+    with open(output_path, 'wb') as f:
+      f.write(image_bytes.getvalue())
